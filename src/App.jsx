1fdb07d3695f2faaf1e1ea7dbc8697b70afdeba1
anjelaaaaaaaa <sewoni@naver.com>
2024-05-24 17:21:49 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+import PokeCard from './components/PokeCard';
+
+function App() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0';
+
+  // api요청 -> response옴 -> state 업뎃 -> component 재랜더링 -> state 
+  useEffect(()=> {
+    fetchPokeData();
+  }, []);
+
+  const fetchPokeData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response.data.results);
+      setPokemons(response.data.results);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  return (
+     <article className='pt-6'>
+      <header className='flex flex-col gap-2 w-full px-4 z-50'>
+        Input form
+      </header>
+      <section className='pt-6 flex flex-col justify-content items-center overflow-auto z-0'>
+        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
+          {pokemons.length > 0 ? 
+          (
+            pokemons.map(({url, name}, index) => (
+              <div>
+                <PokeCard key={url} url={url} name={name}/>
+              </div>
+            ))
+          ) : 
+          (
+            <h2 className='font-medium text-lg text-slate-900 mb-1'>
+              포켓몬이 없습니다.
+            </h2>
+          )}
+        </div>
+      </section>
+     </article>
+  )
+}
+
+export default App
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokeCard = ({url, name}) => {
+  return (
+    <div>PokeCard</div>
+  )
+}
+
+export default PokeCard
